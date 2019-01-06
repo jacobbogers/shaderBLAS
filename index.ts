cc3ac491107877c8b20408c5db9b4dd627305b5f
jacobbogers <jkfbogers@gmail.com>
2019-01-06 23:30:52 +0100

    stats.json added to gitignore

--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
 
 
+(function start() {
+    const canvas: HTMLCanvasElement = <any>document.getElementById('gpu')
+    const gl: WebGLRenderingContext = <WebGLRenderingContext>canvas.getContext('webgl2')
+    if (!gl) {
+        console.log('%c no webgl2', 'color:red')
+        return
+    }
+    const ext = gl.getExtension('EXT_color_buffer_float')
+    if (!ext) {
+        console.log('%c your webgl2 doesnt support rendering to 32bit textures', 'color:red')
+    }
+    console.log('webgl2 rendering 32float fully operational')
+})()
 
-console.log('hello world');
+
