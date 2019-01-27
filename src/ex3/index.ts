4873c9d138388e01ec0aeb132cebe54c8d81d324
jacobbogers <jkfbogers@gmail.com>
2019-01-28 00:20:20 +0100

    reobust loading of jpg via xhr blob

--- src/ex3/index.ts
+++ src/ex3/index.ts
@@ -1,20 +1,16 @@
 import * as debug from 'debug'
-import { createContext  } from '../tools/tools'
+import { createContext, loadImage } from '../tools/tools'
 
-import fs from './fs.glsl'
-import vs from './vs.glsl'
-import pic from '../assets/leaves.jpg'
+const fs = require('./fs.glsl')
+const vs = require('./vs.glsl')
+const pic: string = require('../assets/leaves.jpg')
 
 const log = debug('example3')
 
-
-
-function start(){
+async function start() {
   const ctx = createContext()
-  log('%o', ctx);
-  log('leaves: %s', pic)
-  log('frag shader %s', fs)
-  log('vect shader %s', vs)
+  const jpeg = await loadImage(pic);
+  
 }
 
 window.addEventListener('load', start)
