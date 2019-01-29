b83830c10ac9b99502233487b07932c58a50c89c
jacobbogers <jkfbogers@gmail.com>
2019-01-29 13:02:24 +0100

    nice tooling so far

--- src/ex3/index.ts
+++ src/ex3/index.ts
@@ -1,5 +1,5 @@
 import * as debug from 'debug'
-import { createContext, loadImage } from '../tools/tools'
+import { createGLContext, loadImage } from '../tools/tools'
 
 const fs: string = require('./fs.glsl')
 const vs: string = require('./vs.glsl')
@@ -11,29 +11,12 @@ async function start() {
 
   const jpeg = await loadImage(pic);
 
-  const [ctx, err] = createContext()
+  const [ctx, err] = createGLContext()
   if (err) {
     log(`%c ${err}`, 'color:red')
     return
   }
 
-
-
-  const [program, error] = ctx.compileProgram(vs, fs)
-  if (error) {
-    log(`%c ${error}`, 'color:red')
-    return
-  }
-  log(`%c we have a program ${program}`, 'color:green')
-  
-  var positionAttributeLocation = ctx.gl.getAttribLocation(program, "a_position");
-  console.log(positionAttributeLocation)
-  const attributes = ctx.getLocations(program,'attribute', ['a_position','a_texCoord'])
-  //const uniforms = ctx.getLocations(program, 'uniform', ['u_resolution', 'u_image', 'u_kernel[0]', 'u_kernelWeight','u_flipY'])
-
-
-  //log('uniforms: %o', uniforms)
-  //log('attributes: %o', attributes)
 }
 
 window.addEventListener('load', start)
--- src/tools/tools.ts
+++ src/tools/tools.ts
@@ -1,162 +1,199 @@
-
-export type createContextReturn = {
-    supported: string[],
-    ext: string[],
-    canvas: HTMLCanvasElement,
-    gl: WebGL2RenderingContext,
-    compileProgram(vs: string, fs: string): [WebGLProgram?, string?],
-    getLocations(program: WebGLProgram, type: 'uniform' | 'attribute', names: string[]): [(GLint | WebGLUniformLocation)?, Error?]
+"use strict";
+export interface CreateContextReturn {
+  supported: string[];
+  ext: string[];
+  canvas: HTMLCanvasElement;
+  gl: WebGL2RenderingContext;
 }
 
-export function createContext(extensions: string[] = [], trowIfFail = true): [createContextReturn?, Error?] {
-    const canvas = document.createElement('canvas')
-    if (!canvas) {
-        return [null, new Error(`Could not create canvas`)]
+export interface ProgramContextReturn {
+  program: WebGLProgram;
+  registerUniforms: (names: string[]) => void;
+  registerAttributes: (names: string[]) => void;
+  getAttribute(name: string): GLint;
+  getUniform(name: string): WebGLUniformLocation;
+}
+
+function compileProgram(
+  gl: WebGL2RenderingContext,
+  vs: string,
+  fs: string
+): [WebGLProgram, Error] {
+  function makeShader(type: GLenum, src: string) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, src);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      return [null, new Error(gl.getShaderInfoLog(shader))];
     }
-    const gl = canvas.getContext('webgl2')
-    if (!gl) {
-        return [null, new Error(`No webgl2 available`)]
+    return [shader, null];
+  }
+
+  const errors = [];
+  const [vsh, e1] = makeShader(gl.VERTEX_SHADER, vs);
+  const [fsh, e2] = makeShader(gl.FRAGMENT_SHADER, fs);
+
+  if (e1) errors.push(`[vectorShader: ${e1}]`);
+  if (e2) errors.push(`[fragmentShader: ${e2}]`);
+
+  if (errors.length) {
+    return [null, new Error(errors.join(","))];
+  }
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vsh);
+  gl.attachShader(program, fsh);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    return [null, new Error(gl.getProgramInfoLog(program))];
+  }
+  return [program, null];
+}
+
+export function createGLContext(
+  extensions: string[] = [],
+  trowIfFail = true
+): [CreateContextReturn, Error] {
+  const canvas = document.createElement("canvas");
+  if (!canvas) {
+    return [null, new Error(`Could not create canvas`)];
+  }
+  const gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return [null, new Error(`No webgl2 available`)];
+  }
+
+  const registered = extensions.map<[string, any]>(extension => [
+    extension,
+    gl.getExtension(extension)
+  ]);
+  const errors = registered.filter(f => !f[1]).map(f => f[0]); // get the "falsies"
+
+  if (errors.length) {
+    if (trowIfFail) {
+      return [
+        null,
+        new Error(
+          `These extentions could not be registered:${JSON.stringify(errors)}`
+        )
+      ];
     }
-    const registered = extensions.map<[string, any]>(extension => [extension, gl.getExtension(extension)])
-    const errors = registered.filter(f => !f[1]).map(f => f[0]) // get the "falsies"
-    if (errors.length) {
-        if (trowIfFail) {
-            return [null, new Error(`These extentions could not be registered:${JSON.stringify(errors)}`)]
-        }
+  }
+
+  const supported = gl.getSupportedExtensions();
+  const ext = new Map(registered);
+  const rc = Object.defineProperties(
+    {},
+    {
+      supported: {
+        value: Object.freeze(supported),
+        writeable: false,
+        configurable: false,
+        enumerable: false
+      },
+      ext: {
+        value: Object.freeze(ext),
+        writeable: false,
+        configurable: false,
+        enumerable: false
+      },
+      canvas: {
+        value: canvas,
+        writeable: false,
+        configurable: false,
+        enumerable: false
+      },
+      gl: {
+        value: gl,
+        writeable: false,
+        configurable: false,
+        enumerable: true
+      }
     }
+  );
+  return [rc, null];
+}
 
-    function makeShader(type: GLenum, src: string) {
-        const shader = gl.createShader(type);
-        gl.shaderSource(shader, src);
-        gl.compileShader(shader);
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            return [null, gl.getShaderInfoLog(shader)];
-        }
-        return [shader, null];
+export function createProgramContext(
+  ctx: CreateContextReturn,
+  vs: string,
+  fs: string
+): [ProgramContextReturn, Error] {
+  const [program, err] = compileProgram(ctx.gl, vs, fs);
+  if (err) {
+    return [null, err];
+  }
 
+  const attribs = new Map<string, GLint>();
+  const uniforms = new Map<string, WebGLUniformLocation>();
+
+  function registerUniforms(names: string[]) {
+    const { gl } = ctx;
+    for (const name of names) {
+      const loc = gl.getUniformLocation(program, name);
+      if (loc !== undefined || loc !== null) {
+        uniforms.set(name, gl.getUniformLocation(program, name));
+      }
     }
+  }
 
-    function compileProgram(vs: string, fs: string) {
-
-        const errors = [];
-        const [vsh, e1] = makeShader(gl.VERTEX_SHADER, vs);
-        const [fsh, e2] = makeShader(gl.FRAGMENT_SHADER, fs);
-
-        if (e1) errors.push(`[vectorShader:${e1}]`)
-        if (e2) errors.push(`[fragmentShader:${e2}]`)
-
-        if (errors.length) {
-            return [null, errors.join(',')]
-        }
-        // all ok
-        //create program
-        const program = gl.createProgram();
-        //attach and link shaders to the program
-        gl.attachShader(program, vsh);
-        gl.attachShader(program, fsh);
-        gl.linkProgram(program);
-        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-            return [null, gl.getProgramInfoLog(program)];
-        }
-        return [program, null]
+  function registerAttributes(names: string[]) {
+    const { gl } = ctx;
+    for (const name of names) {
+      const loc = gl.getAttribLocation(program, name);
+      if (loc !== undefined || loc !== null) {
+        attribs.set(name, gl.getAttribLocation(program, name));
+      }
     }
+  }
 
+  function getAttribute(name: string) {
+    return attribs.get(name);
+  }
 
+  function getUniform(name: string) {
+    return uniforms.get(name);
+  }
 
-
-
-
-    function getLocations(program: WebGLProgram, type: 'u' | 'a', names: string[]) {
-           const rc = {}
-           
-           for (const name of names){
-               if (type === 'u'){
-                   rc[name] = gl.getUniformLocation(name)
-               }
-               rc[name] = gl.getAttribLocation
-           }
-           const fn = gl.getAttribLocation
-           return gl.getAttribLocation(program, names[0])
-            /*const found = names.reduce((col, name: string) => {
-                col[name] = fn(program, name)
-                return col
-            }, {} as { [index: string]: GLint | WebGLUniformLocation })
-            return [found, null]*/
-        }
-    
-
-    const supported = gl.getSupportedExtensions()
-    const ext = new Map(registered)
-    const rc = Object.defineProperties({}, {
-        supported: {
-            value: Object.freeze(supported),
-            writeable: false,
-            configurable: false,
-            enumerable: false
-        },
-        ext: {
-            value: Object.freeze(ext),
-            writeable: false,
-            configurable: false,
-            enumerable: false
-        },
-        canvas: {
-            value: canvas,
-            writeable: false,
-            configurable: false,
-            enumerable: false
-        },
-        gl: {
-            value: gl,
-            writeable: false,
-            configurable: false,
-            enumerable: true
-        },
-        compileProgram: {
-            value: compileProgram,
-            writeable: false,
-            configurable: false,
-            enumerable: true
-        },
-        getLocations: {
-            value: getLocations,
-            writeable: false,
-            configurable: false,
-            enumerable: true
-        }
-
-
-
-    })
-    return [rc, null]
+  return [
+    {
+      program,
+      registerUniforms,
+      registerAttributes,
+      getAttribute,
+      getUniform
+    },
+    null
+  ];
 }
 
 export async function loadImage(url: string) {
-
-    const xhr = new XMLHttpRequest();
-    xhr.withCredentials = true
-    xhr.overrideMimeType('application/octet-stream')
-    const rc: any = new Promise((resolve, reject) => {
-        xhr.addEventListener("readystatechange", event => {
-            if (xhr.readyState === xhr.DONE) {
-                if (xhr.status >= 200 && xhr.status < 299) {
-                    resolve({ responseType: xhr.responseType, data: xhr.response })
-                }
-                reject(xhr.statusText)
-            }
-        });
-        xhr.addEventListener('error', reject)
-        xhr.addEventListener('timeout', reject)
-        xhr.open('get', url)
-        xhr.setRequestHeader('Content-Type', 'application/octet-stream')
-        xhr.responseType = 'blob'
-        xhr.send()
-    }).then((response: { responseType: XMLHttpRequestResponseType, data: any }) => {
-        const img = new Image()
-        img.addEventListener('load', () => URL.revokeObjectURL(img.src))
-        img.src = URL.createObjectURL(response.data)
-        return img
-    })
-    rc['xhr'] = xhr
-    return rc
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+  xhr.overrideMimeType("application/octet-stream");
+  const rc: any = new Promise((resolve, reject) => {
+    xhr.addEventListener("readystatechange", event => {
+      if (xhr.readyState === xhr.DONE) {
+        if (xhr.status >= 200 && xhr.status < 299) {
+          resolve({ responseType: xhr.responseType, data: xhr.response });
+        }
+        reject(xhr.statusText);
+      }
+    });
+    xhr.addEventListener("error", reject);
+    xhr.addEventListener("timeout", reject);
+    xhr.open("get", url);
+    xhr.setRequestHeader("Content-Type", "application/octet-stream");
+    xhr.responseType = "blob";
+    xhr.send();
+  }).then(
+    (response: { responseType: XMLHttpRequestResponseType; data: any }) => {
+      const img = new Image();
+      img.addEventListener("load", () => URL.revokeObjectURL(img.src));
+      img.src = URL.createObjectURL(response.data);
+      return img;
+    }
+  );
+  rc["xhr"] = xhr;
+  return rc;
 }
